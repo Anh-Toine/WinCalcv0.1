2ad8ed486133c9bc842b79ee2a6bb6905f6904b9
Manh-Anh Tuan, Nguyen <Nick1234567890a@gmail.com>
2020-11-14 17:00:32 -0500

    Implemented some basic functions(number keys)

--- WinCalcv0.1/MainWindow.xaml.cs
+++ WinCalcv0.1/MainWindow.xaml.cs
@@ -20,10 +20,53 @@ namespace WinCalcv0._1
     /// </summary>
     public partial class MainWindow : Window
     {
+        double firstnum;
+        SelectedOperator selectedOperator;
+
         public MainWindow()
         {
+
             InitializeComponent();
-            //Comment
+
         }
+        public enum SelectedOperator
+        {
+            PLUS,
+            MINUS,
+            MULTIPLY,
+            DIVIDE
+        }
+        //A single event handler
+        private void commonnum_Handler(object sender,RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            string btncont = btn.Content.ToString();
+            
+            if(result.Text == "0")
+            {
+                result.Text = btncont;
+            }
+            else
+            {
+                result.AppendText(btncont);
+            }
+        }
+        void ac_Handler(object sender,RoutedEventArgs e)
+        {
+            Button b = FindName("btn_ac") as Button;
+            b.Click += AC_Click;
+        }
+        private void AC_Click(object sender, RoutedEventArgs e)
+        {
+            result.Text = "0";
+        }
+        
+        //private void btn_ac_Click(object sender, RoutedEventArgs e)
+        //{
+        //    numone = 0;
+        //    numtwo = 0;
+        //    opt = "";
+        //    result.Text = "0";
+        //}
     }
 }
